8f53f883f67df0e2bcc6c88838a6395e336b3e6f
patyumi <pati.yumi@yahoo.com.br>
2023-03-13 21:15:06 -0300

    first commit

---
+++ 1/main.go
@@ -0,0 +1,9 @@
+// o main é o principal package do sistema
+// package de entrada do programa
+
+// todos os outros pacotes devem acompanhar o nome da pasta do pacote
+package main
+
+func main() {
+	println("Hello, World!!")
+}
