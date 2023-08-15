0d444dccd2851d077e920a65c28f85459a8c769c
patyumi <pati.yumi@yahoo.com.br>
2023-08-15 16:42:06 -0300

    graphql

---
+++ 10graphQl/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/patyumi/go-expert/10graphQl/internal/database"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CategoryDB *database.Category
+}
